779fc2b36b90f2ca3cdeef9ca6cb7176685d2cf4
josehm7 <josehernandezmolina700@gmail.com>
2024-10-30 18:30:12 +0100

    Segundo commit

--- insertar.php
+++ insertar.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "sql101.infinityfree.com"; 
-    $username = "if0_37433674"; //Aquí tienes que poner tu usuario        
-    $password = "q0XkSDtiWCUKDTa"; //Aquí tienes que poner tu pass            
-    $dbname = "if0_37433674_Prueba"; //Aquí tienes que poner el nombre de tu BD      
+    $servername = "localhost"; 
+    $username = "jose"; //Aquí tienes que poner tu usuario        
+    $password = "jose"; //Aquí tienes que poner tu pass            
+    $dbname = "prueba"; //Aquí tienes que poner el nombre de tu BD      
 
     // Crear conexión con la base de datos
     $conn = new mysqli($servername, $username, $password, $dbname);
